da30aa19c105d0ce1567deeac35f087afd57e5e5
patak <583075+patak-dev@users.noreply.github.com>
2024-06-29 10:13:53 +0200

    feat: use table for resources (#8)

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,6 +4,7 @@ import DefaultTheme from 'vitepress/theme'
 import TwoslashFloatingVue from '@shikijs/vitepress-twoslash/client'
 import '@shikijs/vitepress-twoslash/style.css'
 import './styles/vars.css'
+import './styles/custom.css'
 
 export default {
   extends: DefaultTheme,
