5c1987d778a5d8f3ced2da969e588eae9e31ad2c
AFCMS <afcm.contact@gmail.com>
2026-06-24 21:29:42 +0200

    Add Vitepress `llms.txt` plugin

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import process from 'node:process'
 import { transformerTwoslash } from '@shikijs/vitepress-twoslash'
 import { defineConfig } from 'vitepress'
 import { groupIconMdPlugin, groupIconVitePlugin } from 'vitepress-plugin-group-icons'
+import llmstxt from 'vitepress-plugin-llms'
 import { buildEnd } from './buildEnd.config'
 
 const ogTitle = 'e18e'
@@ -174,6 +175,7 @@ export default defineConfig({
   vite: {
     plugins: [
       groupIconVitePlugin(),
+      llmstxt(),
     ],
   },
   buildEnd,
